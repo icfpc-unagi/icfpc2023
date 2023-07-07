eef6fa5e3702007b5e0194686ff825b41d534563
sulume <sulume@gmail.com>
2023-07-07 23:26:44 +0900

    api example: submissions

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ pub mod scoring;
 pub use scoring::*;
 use serde::{Deserialize, Serialize};
 
+pub mod api;
+
 pub trait SetMinMax {
     fn setmin(&mut self, v: Self) -> bool;
     fn setmax(&mut self, v: Self) -> bool;
