6f566b0fffcb5a156e6bc7d2c5764285e2f00618
imos <173752+imos@users.noreply.github.com>
2023-07-09 12:56:25 +0000

    .

--- src/bin/optimize.rs
+++ src/bin/optimize.rs
@@ -3,10 +3,10 @@ use icfpc2023::{compute_score_for_instruments, read_input, read_output_from_file
 fn main() {
     let input = read_input();
     let output = read_output_from_file(&std::env::args().nth(1).unwrap());
-    let score_pos_inst = compute_score_for_instruments(&input, &output);
-    let mut ws = mat![0; input.musicians.len(); output.len()];
+    let score_pos_inst = compute_score_for_instruments(&input, &output.0);
+    let mut ws = mat![0; input.musicians.len(); output.0.len()];
     for i in 0..input.musicians.len() {
-        for j in 0..output.len() {
+        for j in 0..output.0.len() {
             ws[i][j] = score_pos_inst[j][input.musicians[i]];
         }
     }
@@ -16,9 +16,9 @@ fn main() {
     if old_score < score {
         eprintln!("Update!!!!!!!!!!!!!!!!!!");
     }
-    let mut out = vec![];
+    let mut out = (vec![], output.1);
     for i in 0..input.musicians.len() {
-        out.push(output[to[i]]);
+        out.0.push(output.0[to[i]]);
     }
     write_output(&out);
 }
