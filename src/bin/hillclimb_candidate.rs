6522c6788dd3dd9be05697e67a401ff594e60a51
Takuya Akiba <t.akiba.65536@gmail.com>
2023-07-09 19:46:30 +0900

    candidate set概ねできたかもしれない

--- src/bin/hillclimb_candidate.rs
+++ src/bin/hillclimb_candidate.rs
@@ -20,35 +20,57 @@ fn main() {
     let mut scorer = DynamicScorer::new_with_output(&input, &output);
     dbg!(scorer.get_score());
 
-    'outer: loop {
-        let score_old = scorer.get_score();
-        let candidate_poss = candidate_positions::enumerate_candidate_positions(&input, &output);
+    loop {
+        let candidate_poss = candidate_positions::enumerate_candidate_positions_with_config(
+            &input,
+            &output,
+            &candidate_positions::CandidateConfig {
+                use_pattern1: true,
+                use_pattern2: true,
+                use_pattern3: true,
+                use_pattern4: true,
+                use_pattern23: false,
+                limit_pattern2: Some(100),
+                limit_pattern3: Some(10),
+                limit_pattern23: None,
+                filter_by_reach: true,
+                pattern2_disallow_blocked: true,
+            },
+        );
         dbg!(candidate_poss.len());
         assert!(candidate_poss.len() > 0);
 
-        let mut musician_ids: Vec<usize> = (0..input.n_musicians()).collect();
-        musician_ids.shuffle(&mut rng);
+        let mut updated = false;
+        'outer: loop {
+            let score_old = scorer.get_score();
+            let mut musician_ids: Vec<usize> = (0..input.n_musicians()).collect();
+            musician_ids.shuffle(&mut rng);
 
-        for musician_id in musician_ids {
-            let pos_old = output[musician_id];
-            for pos in &candidate_poss {
-                output[musician_id] = *pos;
-                if !is_valid_output(&input, &output, false) {
-                    continue;
+            for musician_id in musician_ids {
+                let pos_old = output[musician_id];
+                for pos in &candidate_poss {
+                    output[musician_id] = *pos;
+                    if !is_valid_output(&input, &output, false) {
+                        continue;
+                    }
+                    scorer.move_musician(musician_id, *pos);
+                    let score_new = scorer.get_score();
+                    if score_new > score_old {
+                        println!("{} {:10} -> {:10}", musician_id, score_old, score_new);
+                        dbg!(compute_score(&input, &output));
+                        updated = true;
+                        continue 'outer;
+                    }
                 }
-                scorer.move_musician(musician_id, *pos);
-                let score_new = scorer.get_score();
-                if score_new > score_old {
-                    println!("{} {:10} -> {:10}", musician_id, score_old, score_new);
-                    dbg!(compute_score(&input, &output));
-                    continue 'outer;
-                }
+                scorer.move_musician(musician_id, pos_old);
+                output[musician_id] = pos_old;
             }
-            scorer.move_musician(musician_id, pos_old);
-            output[musician_id] = pos_old;
+            break;
         }
 
-        break;
+        if !updated {
+            break;
+        }
     }
 
     write_output_to_file(&output, "tmp.json");
--- src/candidate_positions.rs
+++ src/candidate_positions.rs
@@ -1,5 +1,18 @@
 use super::*;
 
+pub struct CandidateConfig {
+    pub use_pattern1: bool,
+    pub use_pattern2: bool,
+    pub use_pattern3: bool,
+    pub use_pattern4: bool,
+    pub use_pattern23: bool,
+    pub limit_pattern2: Option<usize>,
+    pub limit_pattern3: Option<usize>,
+    pub limit_pattern23: Option<usize>,
+    pub filter_by_reach: bool,
+    pub pattern2_disallow_blocked: bool,
+}
+
 pub fn is_blocked_by_existing(input: &Input, output: &Output, p: P, q: P) -> bool {
     for r in output {
         if is_blocked(p, q, *r) {
@@ -129,6 +142,67 @@ pub fn pattern3(input: &Input, output: &Output) -> Vec<P> {
     cposs
 }
 
+/// 岩田さんいわく２と３は一緒に生成してほしいらしい
+pub fn pattern23(input: &Input, output: &Output, config: &CandidateConfig) -> Vec<P> {
+    let eps = 1e-6;
+
+    let mut am_pairs = vec![];
+    for attendee_id in 0..input.n_attendees() {
+        for musician_id in 0..input.n_musicians() {
+            if is_blocked_by_someone(input, output, musician_id, attendee_id) {
+                continue;
+            }
+            let d = (input.pos[attendee_id] - output[musician_id]).abs2();
+            am_pairs.push((d, attendee_id, musician_id))
+        }
+    }
+    am_pairs.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+
+    let mut cposs = vec![];
+    for (_, attendee_id, musician_id) in am_pairs {
+        let attendee_pos = input.pos[attendee_id];
+        let musician_pos = output[musician_id];
+
+        // Pattern 2
+        for p in P::tan_cp((musician_pos, 5.0 + eps), attendee_pos) {
+            for q in P::pi_cl((musician_pos, 10.0 + eps), (attendee_pos, p)) {
+                // attendee_pos -> q がブロックされてたらこれいらないでしょっていう
+                if config.pattern2_disallow_blocked
+                    && is_blocked_by_existing(input, output, attendee_pos, q)
+                {
+                    continue;
+                }
+
+                // musicianより更に近くにおけるんだったら何かがおかしい（意味ない）
+                if (q - attendee_pos).abs2() < (musician_pos - attendee_pos).abs2() {
+                    continue;
+                }
+
+                cposs.push(q);
+            }
+        }
+
+        // Pattern 3
+        let vec = (musician_pos - attendee_pos).rot();
+        let vec = vec * ((5.0 + eps) / vec.abs());
+        for stage_side in 0..4 {
+            let stage_line = get_stage_line(input, stage_side);
+
+            for sign in [-1.0, 1.0] {
+                let am_line = (attendee_pos + vec * sign, musician_pos + vec * sign);
+                if let Some(p) = P::pi_ll(stage_line, am_line) {
+                    // 線分と近くないと候補点として意味ない
+                    let d = P::dist2_sp((attendee_pos, musician_pos), p).sqrt();
+                    if d < 5.0 + eps * 10.0 {
+                        cposs.push(p);
+                    }
+                }
+            }
+        }
+    }
+    cposs
+}
+
 /// stage際とmusicianに接するやつ
 pub fn pattern4(input: &Input, output: &Output) -> Vec<P> {
     let eps = 1e-6;
@@ -141,16 +215,6 @@ pub fn pattern4(input: &Input, output: &Output) -> Vec<P> {
         }
     }
     cposs
-}
-
-pub struct CandidateConfig {
-    pub use_pattern1: bool,
-    pub use_pattern2: bool,
-    pub use_pattern3: bool,
-    pub use_pattern4: bool,
-    pub limit_pattern2: Option<usize>,
-    pub limit_pattern3: Option<usize>,
-    pub filter_by_reach: bool,
 }
 
 pub fn filter(mut cp: Vec<P>, input: &Input, output: &Output, config: &CandidateConfig) -> Vec<P> {
@@ -175,6 +239,7 @@ pub fn enumerate_candidate_positions_with_config(
     let mut cp2 = vec![];
     let mut cp3 = vec![];
     let mut cp4 = vec![];
+    let mut cp23 = vec![];
 
     // Generation
     if config.use_pattern1 {
@@ -195,18 +260,35 @@ pub fn enumerate_candidate_positions_with_config(
     if config.use_pattern4 {
         cp4 = filter(pattern4(input, output), input, output, config);
     }
+    if config.use_pattern23 {
+        cp23 = filter(pattern23(input, output, config), input, output, config);
+        if let Some(limit) = config.limit_pattern23 {
+            cp23 = cp23.into_iter().take(limit).collect();
+        }
+    }
     eprintln!(
-        "Candidate sets: {} {} {} {}",
+        "Candidate sets: {} + {} + {} + {} + {}",
         cp1.len(),
         cp2.len(),
         cp3.len(),
-        cp4.len()
+        cp4.len(),
+        cp23.len(),
     );
 
-    cp1.into_iter().chain(cp2).chain(cp3).chain(cp4).collect()
+    cp1.into_iter()
+        .chain(cp2)
+        .chain(cp3)
+        .chain(cp4)
+        .chain(cp23)
+        .collect()
 }
 
-pub fn enumerate_candidate_positions(input: &Input, output: &Output) -> Vec<P> {
+///////////////////////////////////////////////////////////////////////////////
+// プリセット
+///////////////////////////////////////////////////////////////////////////////
+
+/// これは試運転山登り用設定のサンプル
+pub fn enumerate_candidate_positions_hc(input: &Input, output: &Output) -> Vec<P> {
     enumerate_candidate_positions_with_config(
         input,
         output,
@@ -215,9 +297,32 @@ pub fn enumerate_candidate_positions(input: &Input, output: &Output) -> Vec<P> {
             use_pattern2: true,
             use_pattern3: true,
             use_pattern4: true,
+            use_pattern23: false,
             limit_pattern2: Some(1000),
-            limit_pattern3: Some(1000),
+            limit_pattern3: Some(100),
+            limit_pattern23: None,
             filter_by_reach: true,
+            pattern2_disallow_blocked: true,
+        },
+    )
+}
+
+/// これは岩田さん用設定のサンプル
+pub fn enumerate_candidate_positions_sa(input: &Input, output: &Output) -> Vec<P> {
+    enumerate_candidate_positions_with_config(
+        input,
+        output,
+        &CandidateConfig {
+            use_pattern1: true,
+            use_pattern2: false,
+            use_pattern3: false,
+            use_pattern4: true,
+            use_pattern23: true,
+            limit_pattern2: None,
+            limit_pattern3: None,
+            limit_pattern23: Some(2000),
+            filter_by_reach: false,
+            pattern2_disallow_blocked: false,
         },
     )
 }
