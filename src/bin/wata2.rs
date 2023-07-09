d0a5c88214d6ea76341f8908a19d1c30ce318031
wata <wata@atcoder.jp>
2023-07-10 02:06:17 +0900

    候補を追加して焼きなましを繰り返す

--- src/bin/wata2.rs
+++ src/bin/wata2.rs
@@ -3,7 +3,7 @@
 use icfpc2023::{candidate::get_all_candidate, mcf::weighted_matching_with_capacity, *};
 use rand::prelude::*;
 use rayon::prelude::*;
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet};
 
 // ステージ外の点について、一番近いミュージシャンまでの距離の二乗
 pub fn compute_dist2_from_stage(input: &Input, p: P) -> f64 {
@@ -76,7 +76,7 @@ fn preprocess(mut input: Input) -> Input {
 }
 
 // 候補点を追加する
-fn add_cand(input: &Input, cand_list: &mut Vec<P>) {
+fn add_cand(input: &Input, cand_list: &mut Vec<P>, has_ans: bool) {
     let stage0 = P(input.stage0.0 + 10.0, input.stage0.1 + 10.0);
     let stage1 = P(input.stage1.0 - 10.0, input.stage1.1 - 10.0);
     if std::env::var("CAND_CHOKUDAI")
@@ -86,48 +86,62 @@ fn add_cand(input: &Input, cand_list: &mut Vec<P>) {
     {
         cand_list.extend(get_all_candidate(input));
     }
-    for d in 0.. {
-        let x = stage0.0 + 10.0 * d as f64;
-        if x > stage1.0 {
-            break;
+    if !has_ans {
+        for d in 0.. {
+            let x = stage0.0 + 10.0 * d as f64;
+            if x > stage1.0 {
+                break;
+            }
+            cand_list.push(P(x, stage0.1));
+            cand_list.push(P(x, stage1.1));
+            cand_list.push(P(stage1.0 - 10.0 * d as f64, stage0.1));
+            cand_list.push(P(stage1.0 - 10.0 * d as f64, stage1.1));
         }
-        cand_list.push(P(x, stage0.1));
-        cand_list.push(P(x, stage1.1));
-        cand_list.push(P(stage1.0 - 10.0 * d as f64, stage0.1));
-        cand_list.push(P(stage1.0 - 10.0 * d as f64, stage1.1));
-    }
-    for d in 0.. {
-        let y = stage0.1 + 10.0 * d as f64;
-        if y > stage1.1 {
-            break;
+        for d in 0.. {
+            let y = stage0.1 + 10.0 * d as f64;
+            if y > stage1.1 {
+                break;
+            }
+            cand_list.push(P(stage0.0, y));
+            cand_list.push(P(stage1.0, y));
+            cand_list.push(P(stage0.0, stage1.1 - 10.0 * d as f64));
+            cand_list.push(P(stage1.0, stage1.1 - 10.0 * d as f64));
         }
-        cand_list.push(P(stage0.0, y));
-        cand_list.push(P(stage1.0, y));
-        cand_list.push(P(stage0.0, stage1.1 - 10.0 * d as f64));
-        cand_list.push(P(stage1.0, stage1.1 - 10.0 * d as f64));
-    }
-    // 内側に十分な量を追加しておく
-    let mut count = 0;
-    for w in 0i32.. {
-        for d1 in -w..=w {
-            for d2 in -w..=w {
-                if d1.abs() == w || d2.abs() == w {
-                    let x = (stage0.0 + stage1.0) * 0.5 + 10.0 * d1 as f64;
-                    let y = (stage0.1 + stage1.1) * 0.5 + 10.0 * d2 as f64;
-                    if stage0.0 <= x && x <= stage1.0 && stage0.1 <= y && y <= stage1.1 {
-                        cand_list.push(P(x, y));
-                        count += 1;
+        // 内側に十分な量を追加しておく
+        let mut count = 0;
+        for w in 0i32.. {
+            for d1 in -w..=w {
+                for d2 in -w..=w {
+                    if d1.abs() == w || d2.abs() == w {
+                        let x = (stage0.0 + stage1.0) * 0.5 + 10.0 * d1 as f64;
+                        let y = (stage0.1 + stage1.1) * 0.5 + 10.0 * d2 as f64;
+                        if stage0.0 <= x && x <= stage1.0 && stage0.1 <= y && y <= stage1.1 {
+                            cand_list.push(P(x, y));
+                            count += 1;
+                        }
                     }
                 }
             }
-        }
-        if count >= input.musicians.len() {
-            break;
+            if count >= input.musicians.len() {
+                break;
+            }
         }
     }
     cand_list.sort();
     cand_list.dedup();
     eprintln!("#cand = {}", cand_list.len());
+}
+
+/// 現在の解の周辺に候補点を拡張する
+fn extend_cand(input: &Input, cand: &mut Vec<P>, best: &Vec<P>) {
+    let volumes = vec![10.0; best.len()];
+    cand.extend(candidate_positions::enumerate_candidate_positions_sa(
+        input,
+        &(best.clone(), volumes),
+    ));
+    cand.sort();
+    cand.dedup();
+    eprintln!("#cand = {}", cand.len());
 }
 
 // ceilは取らない
@@ -155,24 +169,15 @@ impl State {
     fn initialize(
         input: &Input,
         cand: &Vec<P>,
+        mut block_count: Vec<Vec<i32>>,
         block: &Vec<Vec<(usize, usize)>>,
         to: &Vec<usize>,
     ) -> Self {
-        let mut block_count = mat![0; cand.len(); input.n_attendees()];
         let mut musicians = vec![!0; cand.len()];
         let to = to.clone();
         let mut coef_musicians = vec![0.0; input.n_musicians()];
         let mut closeness = vec![0.0; input.n_musicians()];
         let mut score = 0.0;
-        for i in 0..cand.len() {
-            for a in 0..input.n_attendees() {
-                for &p in &input.pillars {
-                    if is_blocked_by_circle(cand[i], input.pos[a], p) {
-                        block_count[i][a] += 1;
-                    }
-                }
-            }
-        }
         for i in 0..input.musicians.len() {
             musicians[to[i]] = i;
             for &(p, j) in &block[to[i]] {
@@ -365,13 +370,14 @@ impl State {
             true
         }
     }
-    fn get_output(&self) -> (Vec<usize>, Vec<f64>) {
+    fn get_output(&self, cand: &Vec<P>) -> Output {
+        let pos = self.to.iter().map(|&p| cand[p]).collect();
         let volume = self
             .coef_musicians
             .iter()
             .map(|&v| if v > 0.0 { 10.0 } else { 0.0 })
             .collect();
-        (self.to.clone(), volume)
+        (pos, volume)
     }
 }
 
@@ -392,139 +398,185 @@ fn main() {
     cand.sort();
     cand.dedup();
     eprintln!("#candidates from ansfiles = {}", cand.len());
-    add_cand(&input, &mut cand);
-    // extend_cand(&input, &mut cand);
-    let mut near = vec![vec![]; cand.len()];
-    let mut conflict = vec![vec![]; cand.len()];
-    for i in 0..cand.len() {
-        let mut tmp = vec![];
-        for j in 0..cand.len() {
-            let d2 = (cand[i] - cand[j]).abs2();
-            if d2 < 100.0 {
-                conflict[i].push(j);
-            }
-            tmp.push((d2, j));
-        }
-        tmp.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
-        tmp.truncate(20);
-        near[i] = tmp;
+    let start_from_ans = std::env::var("START_FROM_ANS")
+        .unwrap_or(String::new())
+        .len()
+        > 0;
+    add_cand(&input, &mut cand, start_from_ans);
+    let REP = std::env::var("REP")
+        .map(|r| r.parse().unwrap())
+        .unwrap_or(1);
+    let mut best = vec![];
+    if start_from_ans {
+        best = read_output_from_file(&std::env::args().nth(3).unwrap()).0;
     }
-    let block = (0..cand.len())
-        .into_par_iter()
-        .map(|i| {
+    for rep in 0..REP {
+        eprintln!("========== {} ==========", rep);
+        if !best.is_empty() {
+            extend_cand(&input, &mut cand, &best);
+        }
+        let mut block_count = mat![0; cand.len(); input.n_attendees()];
+        for i in 0..cand.len() {
+            for a in 0..input.n_attendees() {
+                for &p in &input.pillars {
+                    if is_blocked_by_circle(cand[i], input.pos[a], p) {
+                        block_count[i][a] += 1;
+                    }
+                }
+            }
+        }
+        let mut near = vec![vec![]; cand.len()];
+        let mut conflict = vec![vec![]; cand.len()];
+        for i in 0..cand.len() {
             let mut tmp = vec![];
             for j in 0..cand.len() {
-                if (cand[i] - cand[j]).abs2() >= 100.0 {
-                    for k in 0..input.pos.len() {
-                        if is_blocked(cand[j], input.pos[k], cand[i]) {
-                            tmp.push((j, k));
+                let d2 = (cand[i] - cand[j]).abs2();
+                if d2 < 100.0 {
+                    conflict[i].push(j);
+                }
+                tmp.push((d2, j));
+            }
+            tmp.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+            tmp.truncate(20);
+            near[i] = tmp;
+        }
+        let block = (0..cand.len())
+            .into_par_iter()
+            .map(|i| {
+                let mut tmp = vec![];
+                for j in 0..cand.len() {
+                    if (cand[i] - cand[j]).abs2() >= 100.0 {
+                        for k in 0..input.pos.len() {
+                            if block_count[j][k] == 0 && is_blocked(cand[j], input.pos[k], cand[i])
+                            {
+                                tmp.push((j, k));
+                            }
                         }
                     }
                 }
+                tmp
+            })
+            .collect::<Vec<_>>();
+        let mut rng = rand::thread_rng();
+        let mut to = vec![!0; input.musicians.len()];
+        if !best.is_empty() {
+            let mut id = BTreeMap::new();
+            for i in 0..cand.len() {
+                id.insert(cand[i], i);
             }
-            tmp
-        })
-        .collect::<Vec<_>>();
-    let mut rng = rand::thread_rng();
-    let mut to = vec![!0; input.musicians.len()];
-    if std::env::var("START_FROM_ANS")
-        .unwrap_or(String::new())
-        .len()
-        > 0
-    {
-        let mut id = BTreeMap::new();
-        for i in 0..cand.len() {
-            id.insert(cand[i], i);
-        }
-        let ans = read_output_from_file(&std::env::args().nth(3).unwrap());
-        for i in 0..ans.0.len() {
-            to[i] = id[&ans.0[i]];
-        }
-    } else {
-        let mut used = vec![false; cand.len()];
-        for i in 0..input.musicians.len() {
-            loop {
-                let j = rng.gen_range(0, cand.len());
-                if !used[j] {
-                    to[i] = j;
-                    for &k in &conflict[j] {
-                        used[k] = true;
+            for i in 0..best.len() {
+                to[i] = id[&best[i]];
+            }
+        } else {
+            let mut used = vec![false; cand.len()];
+            for i in 0..input.musicians.len() {
+                loop {
+                    let j = rng.gen_range(0, cand.len());
+                    if !used[j] {
+                        to[i] = j;
+                        for &k in &conflict[j] {
+                            used[k] = true;
+                        }
+                        break;
                     }
-                    break;
                 }
             }
         }
+        let mut state = State::initialize(&input, &cand, block_count, &block, &to);
+        let mut best_score = state.score;
+        eprintln!("{:.3}: {:.0}", get_time(), best_score);
+        let mut sum = 1.0;
+        let mut cnt = 0;
+
+        let TL: f64 = std::env::var("TL")
+            .map(|a| a.parse().unwrap())
+            .unwrap_or(600.0);
+        let stime = get_time();
+        let mut num_used = vec![0i64; cand.len()];
+        let mut mcf_count = 0;
+        let mut mcf_success = 0;
+        for iter in 0.. {
+            if iter & 31 == 0 {
+                for &p in &state.to {
+                    num_used[p] += 1;
+                }
+            }
+            let t = (get_time() - stime) / TL;
+            if t >= 1.0 {
+                eprintln!("Iter = {}", iter);
+                break;
+            }
+            let i1 = state.to[rng.gen_range(0, input.n_musicians())];
+            let i2 = if rng.gen_bool(0.1) {
+                rng.gen_range(0, cand.len())
+            } else {
+                near[i1].choose(&mut rng).unwrap().1
+            };
+            if state.musicians[i2] == !0 {
+                if let Some(diff) = state.mov(&input, &cand, &block, &conflict, i1, i2) {
+                    sum += diff.abs() as f64;
+                    cnt += 1;
+
+                    let ave = sum / cnt as f64;
+                    let mut T = 0.2 * ave * (1.0 - t) * (1.0 - t);
+                    if T <= 1.0 {
+                        T = 1.0;
+                    }
+
+                    if diff >= 0.0 || rng.gen_bool((diff as f64 / T).exp()) {
+                    } else {
+                        state.mov(&input, &cand, &block, &conflict, i2, i1).unwrap();
+                    }
+                }
+            } else {
+                if let Some(diff) = state.swap(&input, &cand, i1, i2) {
+                    sum += diff.abs() as f64;
+                    cnt += 1;
+
+                    let ave = sum / cnt as f64;
+                    let mut T = 0.2 * ave * (1.0 - t) * (1.0 - t);
+                    if T <= 1.0 {
+                        T = 1.0;
+                    }
+                    if diff >= 0.0 || rng.gen_bool((diff as f64 / T).exp()) {
+                    } else {
+                        state.swap(&input, &cand, i1, i2).unwrap();
+                    }
+                }
+            }
+            if best_score + 0.1 < state.score {
+                if (mcf_count < 10 || mcf_success as f64 / mcf_count as f64 > 0.1)
+                    && state.optimize_mcf(&input, &cand)
+                {
+                    mcf_success += 1;
+                    eprintln!("{:.0} -> {:.0}", best_score, state.score);
+                }
+                mcf_count += 1;
+                best_score.setmax(state.score);
+                eprintln!("{:.3}: {:.0}", get_time(), best_score);
+                let out = state.get_output(&cand);
+                write_output_to_file(
+                    &out,
+                    &format!("{}/{}.json", outdir, input.problem_id.unwrap_or(0)),
+                );
+                best = out.0;
+            }
+        }
+        let mut list = vec![];
+        let used = best.iter().collect::<BTreeSet<_>>();
+        for i in 0..cand.len() {
+            if used.contains(&cand[i]) {
+                list.push((-(1i64 << 60), cand[i]));
+            } else {
+                list.push((-num_used[i], cand[i]));
+            }
+        }
+        list.sort();
+        let cn = best.len().max(cand.len() / 2);
+        cand.clear();
+        for i in 0..cn {
+            cand.push(list[i].1);
+        }
+        eprintln!("#cand = {}", cand.len());
     }
-    let mut state = State::initialize(&input, &cand, &block, &to);
-    let mut best = state.get_output();
-    let mut best_score = state.score;
-    eprintln!("{:.3}: {}", get_time(), best_score);
-    let mut sum = 1.0;
-    let mut cnt = 0;
-
-    let TL: f64 = std::env::var("TL")
-        .map(|a| a.parse().unwrap())
-        .unwrap_or(600.0);
-    let stime = get_time();
-    let _num_used = vec![0i64; cand.len()];
-    for iter in 0.. {
-        if iter & 0xff == 0 {}
-        let t = (get_time() - stime) / TL;
-        if t >= 1.0 {
-            eprintln!("Iter = {}", iter);
-            break;
-        }
-        let i1 = state.to[rng.gen_range(0, input.n_musicians())];
-        let i2 = if rng.gen_bool(0.1) {
-            rng.gen_range(0, cand.len())
-        } else {
-            near[i1].choose(&mut rng).unwrap().1
-        };
-        if state.musicians[i2] == !0 {
-            if let Some(diff) = state.mov(&input, &cand, &block, &conflict, i1, i2) {
-                sum += diff.abs() as f64;
-                cnt += 1;
-
-                let ave = sum / cnt as f64;
-                let mut T = ave * (1.0 - t) * (1.0 - t);
-                if T <= 1.0 {
-                    T = 1.0;
-                }
-
-                if diff >= 0.0 || rng.gen_bool((diff as f64 / T).exp()) {
-                } else {
-                    state.mov(&input, &cand, &block, &conflict, i2, i1).unwrap();
-                }
-            }
-        } else {
-            if let Some(diff) = state.swap(&input, &cand, i1, i2) {
-                sum += diff.abs() as f64;
-                cnt += 1;
-
-                let ave = sum / cnt as f64;
-                let mut T = ave * (1.0 - t) * (1.0 - t);
-                if T <= 1.0 {
-                    T = 1.0;
-                }
-                if diff >= 0.0 || rng.gen_bool((diff as f64 / T).exp()) {
-                } else {
-                    state.swap(&input, &cand, i1, i2).unwrap();
-                }
-            }
-        }
-        if best_score + 0.1 < state.score {
-            if state.optimize_mcf(&input, &cand) {
-                eprintln!("{:.0} -> {:.0}", best_score, state.score);
-            }
-            best_score.setmax(state.score);
-            best = state.get_output();
-            eprintln!("{:.3}: {:.0}", get_time(), best_score);
-        }
-    }
-    let out = best.0.iter().map(|&p| cand[p]).collect();
-    let volumes = best.1;
-    write_output_to_file(
-        &(out, volumes),
-        &format!("{}/{}.json", outdir, input.problem_id.unwrap_or(0)),
-    )
 }
