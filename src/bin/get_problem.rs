a4b2bf0cfa33ae094e216a4241cd36f3156ee188
Kentaro IMAJO <173752+imos@users.noreply.github.com>
2023-07-10 07:41:59 +0900

    feat: implement get_problem (#47)

---
+++ src/bin/get_problem.rs
@@ -0,0 +1,21 @@
+use anyhow::Result;
+use clap::Parser;
+use tokio;
+
+use icfpc2023::api;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[arg(default_value_t = 0)]
+    problem_id: u32,
+}
+
+/// Print a problem from the problem database.
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Cli::parse();
+
+    let problem = api::get_raw_problem_db(args.problem_id).await?;
+    println!("{}", problem);
+    Ok(())
+}
