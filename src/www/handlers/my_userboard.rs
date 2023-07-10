3cbda63dc9690f74f83549c68c3058ecd43f5085
imos <173752+imos@users.noreply.github.com>
2023-07-10 07:09:15 +0000

    .

--- src/www/handlers/my_userboard.rs
+++ src/www/handlers/my_userboard.rs
@@ -49,17 +49,25 @@ NATURAL RIGHT JOIN(
         let submission_score = submission_score.unwrap_or(0);
         total_score += submission_score;
         let score = submission_score.to_formatted_string(&Locale::en);
-        let id = official_id.unwrap_or_else(|| submission_id.to_string());
+        let id = official_id
+            .clone()
+            .unwrap_or_else(|| submission_id.to_string());
         let score = format!("<a href=\"/submission?submission_id={}\">{}</a>", id, score);
         buf.push_str(&format!(
             r#"
 <div style="display: inline-block; min-width: 200px; width: 20%; margin: 1em; text-align: right">
 問題番号: {}<br>
 スコア: {}<br>
+<div style="font-size: 70%">提出ID (#{}): {}</div>
 <a href="/submission?submission_id={}"><img src="/problem_png?problem_id={}" style="width: 200px; height: 200px; object-fit: contain;"></a>
 </div>
 "#,
-            problem_id, score, id, problem_id,
+            problem_id,
+            score,
+            submission_id,
+            official_id.clone().unwrap_or("N/A".into()),
+            id,
+            problem_id,
         ));
     }
     Ok(format!(
