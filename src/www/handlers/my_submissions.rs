654c7a45cb5b7a15ceffa33701d96c443d47111e
imos <173752+imos@users.noreply.github.com>
2023-07-10 08:09:03 +0000

    .

--- src/www/handlers/my_submissions.rs
+++ src/www/handlers/my_submissions.rs
@@ -9,7 +9,7 @@ use serde::Deserialize;
 use std::iter::*;
 use std::{collections::HashMap, fmt::Write};
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct Query {
     #[serde(default = "default_offset")]
     pub offset: u32,
@@ -17,6 +17,10 @@ pub struct Query {
     pub limit: u32,
     #[serde(default)]
     pub tag: String,
+    #[serde(default = "default_problem_id")]
+    pub problem_id: u32,
+    #[serde(default)]
+    pub order_by: String,
 }
 
 fn default_offset() -> u32 {
@@ -25,6 +29,10 @@ fn default_offset() -> u32 {
 
 fn default_limit() -> u32 {
     100
+}
+
+fn default_problem_id() -> u32 {
+    0
 }
 
 // Implement the handler function to list submissions from MySQL database with offset and limit.
@@ -39,6 +47,17 @@ pub async fn handler(info: web::Query<Query>) -> impl Responder {
     response
 }
 
+fn build_url(info: &Query) -> String {
+    format!(
+        "/my_submissions?offset={offset}&limit={limit}&tag={tag}&problem_id={problem_id}&order_by={order_by}",
+        offset = info.offset,
+        limit = info.limit,
+        tag = urlencode(&info.tag),
+        problem_id = info.problem_id,
+        order_by = urlencode(&info.order_by),
+    )
+}
+
 async fn handle(info: &web::Query<Query>) -> Result<String, Box<dyn std::error::Error>> {
     let mut buf = String::new();
     write!(
@@ -46,16 +65,62 @@ async fn handle(info: &web::Query<Query>) -> Result<String, Box<dyn std::error::
         "<a href=\"/submissions\">[Load from the official API]</a>"
     )?;
 
-    write!(
-        &mut buf,
-        "<h1>Submissions from local DB copy (excl. pending submissions)</h1>"
-    )?;
+    if info.problem_id == 0 {
+        write!(&mut buf, "<h1>提出一覧 (DB)</h1>")?;
+    } else {
+        write!(
+            &mut buf,
+            "<h1>問題番号: {problem_id}</h1>",
+            problem_id = info.problem_id
+        )?;
+    }
 
     buf.push_str("<table>");
 
-    let rows = if info.tag.is_empty() {
+    let order_by = match info.order_by.as_str() {
+        "submission_score" => "submission_score",
+        _ => "submission_created",
+    };
+
+    let order_by_options = &[
+        ("submission_created", "日付順"),
+        ("submission_score", "スコア順"),
+    ];
+    // Enable to select order_by.
+    buf.push_str("<div>ソート順:");
+    for (column, name) in order_by_options {
+        if order_by == column.deref() {
+            write!(&mut buf, " [{name}]", name = name)?;
+        } else {
+            write!(
+                &mut buf,
+                " [<a href=\"{url}\">{name}</a>]",
+                url = build_url(&Query {
+                    order_by: column.to_string(),
+                    ..info.deref().clone()
+                }),
+                name = name
+            )?;
+        }
+    }
+    buf.push_str("</div>");
+
+    let rows = {
+        let mut where_clause = Vec::new();
+        if !info.tag.is_empty() {
+            where_clause.push("submission_tag = :tag");
+        }
+        if info.problem_id != 0 {
+            where_clause.push("problem_id = :problem_id");
+        }
+        let where_clause = if where_clause.is_empty() {
+            String::new()
+        } else {
+            format!("WHERE {}", where_clause.join(" AND "))
+        };
         sql::select(
-            r#"
+            &format!(
+                r#"
 SELECT
     submission_id,
     official_id,
@@ -65,39 +130,18 @@ SELECT
     DATE_FORMAT(submission_created, "%Y-%m-%d %T") AS submission_created
 FROM
     submissions
+{where_clause}
 ORDER BY
-    submission_created
+    {order_by}
 DESC
 LIMIT :offset, :limit
 "#,
-            params! {
-                "offset" => info.offset,
-                "limit" => info.limit
-            },
-        )?
-    } else {
-        sql::select(
-            r#"
-SELECT
-    submission_id,
-    official_id,
-    problem_id,
-    submission_score,
-    submission_error,
-    DATE_FORMAT(submission_created, "%Y-%m-%d %T") AS submission_created
-FROM
-    submissions
-NATURAL JOIN
-    submission_tags
-WHERE
-    submission_tag = :tag
-ORDER BY
-    submission_created
-DESC
-LIMIT :offset, :limit
-"#,
+                where_clause = where_clause,
+                order_by = order_by
+            ),
             params! {
                 "tag" => &info.tag,
+                "problem_id" => info.problem_id,
                 "offset" => info.offset,
                 "limit" => info.limit
             },
@@ -160,23 +204,34 @@ WHERE
                 for tag in tags {
                     write!(
                         &mut tag_str,
-                        "<a href=\"/my_submissions?tag={}\" class=\"tag\">{}</a>",
-                        percent_encoding::utf8_percent_encode(
-                            tag,
-                            percent_encoding::NON_ALPHANUMERIC
-                        ),
-                        tag
+                        "<a href=\"{url}\" class=\"tag\">{tag}</a>",
+                        url = build_url(&Query {
+                            tag: tag.clone(),
+                            ..info.deref().clone()
+                        }),
+                        tag = tag
                     )?;
                 }
             }
             Ok(format!(
-                "<tr><td><a href=\"/submission?submission_id={}\">{}</a></td><td>{}</td><td class=\"align-r\">{}</td><td class=\"align-r\">{}</td><td>{}</td></tr>",
-                submission_id,
-                official_id.unwrap_or("N/A".into()),
-                maybe_enrich_datetime_str(submission_created),
-                problem_id,
-                score,
-                tag_str,
+                r#"
+<tr>
+<td><a href="/submission?submission_id={submission_id}">{official_id}</a></td>
+<td>{submission_created}</td>
+<td class="align-r"><a href="{problem_url}">{problem_id}</a></td>
+<td class="align-r">{score}</td>
+<td>{tags}</td>
+</tr>"#,
+                submission_id = submission_id,
+                official_id = official_id.unwrap_or("N/A".into()),
+                submission_created = maybe_enrich_datetime_str(submission_created),
+                problem_url = build_url(&Query {
+                    problem_id: problem_id,
+                    ..info.deref().clone()
+                }),
+                problem_id = problem_id,
+                score = score,
+                tags = tag_str,
             ))
         }() {
             Ok(s) => {
@@ -192,9 +247,12 @@ WHERE
 
     write!(
         &mut buf,
-        "<a href=\"/my_submissions?offset={}&limit={}\">Next</a>",
-        info.offset + info.limit,
-        info.limit
+        "<a href=\"{url}\">Next</a>",
+        url = build_url(&Query {
+            offset: info.offset + info.limit,
+            limit: info.limit,
+            ..info.deref().clone()
+        }),
     )?;
 
     Ok(buf)
